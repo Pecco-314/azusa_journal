456e92f687d279027a1acb07e3cb53bab568e9d0
Pecco <pecco@qq.com>
2022-01-08 23:04:36 +0800

    增加排名变化功能

--- get_rank.py
+++ get_rank.py
@@ -10,21 +10,46 @@ def get_point(view, coin, favorite, like):
     A = coin * 2 + favorite * 2 + like
     return A * min(0.5, A / view) * 200 + view
 
-map = json.load(open(f"data/{sys.argv[1]}.json"))
-videos = []
-for k, v in map.items():
-    videos.append({
-        'aid': k,
-        'view': v['view'],
-        'coin': v['coin'],
-        'favorite': v['favorite'],
-        'like': v['like'],
-        'point': get_point(v['view'], v['coin'], v['favorite'], v['like'])
-    })
-videos.sort(key=lambda k : -k['point'])
+this = json.load(open(f"data/{sys.argv[1]}.json"))
+last = {}
+if len(sys.argv) >= 3:
+    last = json.load(open(f"data/{sys.argv[2]}.json"))
 exclusive = json.load(open("exclusive.json"))
+
+def get_video_list(map):
+    videos = []
+    for k, v in map.items():
+        videos.append({
+            'aid': k,
+            'view': v['view'],
+            'coin': v['coin'],
+            'favorite': v['favorite'],
+            'like': v['like'],
+            'point': get_point(v['view'], v['coin'], v['favorite'], v['like'])
+        })
+    videos.sort(key=lambda k : -k['point'])
+    return videos
+
+this_videos = get_video_list(this)
+last_videos = get_video_list(last)
+for video in last_videos:
+    if video.get('rank') is not None:
+        this_videos['rank'] = '-'
+
+def get_change(last, this):
+    if last is None:
+        return 'New'
+    elif last == '-':
+        return 'Re'
+    elif last == this:
+        return '--'
+    elif last < this:
+        return f'↓{this - last}'
+    else:
+        return f'↑{last - this}'
+
 rk = 0
-for video in videos:
+for video in this_videos:
     if video['aid'] in exclusive:
         continue
     obj = requests.get(URL.format(video['aid'])).json()
@@ -32,7 +57,9 @@ for video in videos:
         continue
     rk += 1
     data = obj['data']
-    print(f"""第{rk}名：{data['title']}（av{data['aid']}）
+    last_video = last.get(video['aid'])
+    last_rank = None if last_video is None else last_video.get('rank')
+    print(f"""第{rk}名（{get_change(last_rank, rk)}）：{data['title']}（av{video['aid']}）
     作者：{data['owner']['name']}
     点数：{video['point']}
     播放量：{video['view']}
@@ -40,5 +67,9 @@ for video in videos:
     收藏：{video['favorite']}
     点赞：{video['like']}
     """)
+    this[video['aid']]['rank'] = rk
     if rk == 25:
         break
+
+with open(f"data/{sys.argv[1]}.json", "w", encoding='utf-8') as fp:
+    json.dump(this, fp)
--- run.py
+++ run.py
@@ -4,8 +4,9 @@ import datetime
 now = datetime.datetime.now()
 t = f"{now:%Y-%m-%d}"
 t2 = f"{now - datetime.timedelta(7):%Y-%m-%d}"
+t3 = f"{now - datetime.timedelta(14):%Y-%m-%d}"
 os.system("python3 update_videos.py")
 os.system(f"python3 get_data.py {t}")
-# if now.weekday() == 1:
-#     os.system("python3 get_delta.py {t2} {t}")
-#     os.system("python3 get_rank.py delta_{t2}_{t} > data/rank_{t2}_{t}")
+if now.weekday() == 1:
+    os.system(f"python3 get_delta.py {t2} {t}")
+    os.system(f"python3 get_rank.py delta_{t2}_{t} delta_{t3}_{t2} > data/rank_{t2}_{t}.txt")
